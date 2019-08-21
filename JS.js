7a906da2541d0665f913ada738a52150af92da0f
RamneetKaur01 <53788254+RamneetKaur01@users.noreply.github.com>
2019-08-21 11:29:24 -0400

    Update JS.js

--- JS.js
+++ JS.js
@@ -26,11 +26,11 @@ function showSlides() {
 function infohide(){
   x = document.getElementById("info");
   if (x.innerHTML == 'Information'){
-    x.innerHTML = 'Hide'
+    x.innerHTML = 'Hide Information'
     document.getElementById('ocdinformation').style.display = "block";
 
   }
-  else if (x.innerHTML == 'Hide'){
+  else if (x.innerHTML == 'Hide Information'){
     x.innerHTML = 'Information';
     document.getElementById('ocdinformation').style.display = "none";
 
@@ -39,12 +39,12 @@ function infohide(){
 
 function symphide(){
   y = document.getElementById("symptom");
-  if (y.innerHTML == 'Symptom'){
-    y.innerHTML = 'Hide'
+  if (y.innerHTML == 'Symptoms'){
+    y.innerHTML = 'Hide Symptoms'
     document.getElementById('ocdsymptom').style.display = "block";
 
   }
-  else if (y.innerHTML == 'Hide'){
+  else if (y.innerHTML == 'Hide Symptoms'){
     y.innerHTML = 'Symptom';
     document.getElementById('ocdsymptom').style.display = "none";
 
@@ -53,14 +53,73 @@ function symphide(){
 
 function leadhide(){
   y = document.getElementById("lead");
-  if (y.innerHTML == 'OCD Leads To'){
-    y.innerHTML = 'Hide'
+  if (y.innerHTML == 'Related'){
+    y.innerHTML = 'Hide Related'
     document.getElementById('ocdlead').style.display = "block";
 
   }
-  else if (y.innerHTML == 'Hide'){
-    y.innerHTML = 'OCD Leads To';
+  else if (y.innerHTML == 'Hide Related'){
+    y.innerHTML = 'Related';
     document.getElementById('ocdlead').style.display = "none";
+
+  }
+}
+
+function symptomhide(){
+  x = document.getElementById("adhdsymptom");
+  if (x.innerHTML == 'Symptoms'){
+    x.innerHTML = 'Hide Symptoms'
+    document.getElementById('adsymptom').style.display = "block";
+
+  }
+  else if (x.innerHTML == 'Hide Symptoms'){
+    x.innerHTML = 'Symptoms';
+    document.getElementById('adsymptom').style.display = "none";
+
+  }
+}
+
+function typehide(){
+  x = document.getElementById("adhdtypes");
+  if (x.innerHTML == 'Types Of ADHD'){
+    x.innerHTML = 'Hide Types'
+    document.getElementById('adtypes').style.display = "block";
+
+  }
+  else if (x.innerHTML == 'Hide Types'){
+    x.innerHTML = 'Types Of ADHD';
+    document.getElementById('adtypes').style.display = "none";
+
+  }
+}
+
+
+
+function causehide(){
+  x = document.getElementById("adhdcauses");
+  if (x.innerHTML == 'Causes Of ADHD'){
+    x.innerHTML = 'Hide Causes'
+    document.getElementById('adcauses').style.display = "block";
+
+  }
+  else if (x.innerHTML == 'Hide Causes'){
+    x.innerHTML = 'Causes Of ADHD';
+    document.getElementById('adcauses').style.display = "none";
+
+  }
+}
+
+
+function preventhide(){
+  x = document.getElementById("adhdprevention");
+  if (x.innerHTML == 'Preventions'){
+    x.innerHTML = 'Hide Preventions'
+    document.getElementById('adprevention').style.display = "block";
+
+  }
+  else if (x.innerHTML == 'Hide Preventions'){
+    x.innerHTML = 'Preventions';
+    document.getElementById('adprevention').style.display = "none";
 
   }
 }
